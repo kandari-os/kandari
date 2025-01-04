7d5d51dab2431b7366c2f0569eaacdea0efd5efa
zihad <tazihad@gmail.com>
2025-01-05 02:54:37 +0600

    refactor: :fire: removed breeze theme fix
    it doesn't work

--- scripts/kinoite/003_bug_fix_breeze_theme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-mkdir -p /var/lib/flatpak/runtime/org.gtk.Gtk3theme.Breeze/x86_64/3.22/active/files/assets/
-
-cp -r /usr/share/themes/Breeze-Dark/assets/* /var/lib/flatpak/runtime/org.gtk.Gtk3theme.Breeze/x86_64/3.22/active/files/assets/
