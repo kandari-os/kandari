389340cccd8bb21553433242850436fc4ebbc254
zihad <tazihad@gmail.com>
2025-04-13 13:13:31 +0600

    change mirror rpmfusion

--- scripts/base/001_rpm_fusion.sh
+++ scripts/base/001_rpm_fusion.sh
@@ -4,8 +4,8 @@ set -euox pipefail
 
 # Setup RPMFusion repositories
 rpm-ostree install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 rpm-ostree install \
   rpmfusion-nonfree-release  \
