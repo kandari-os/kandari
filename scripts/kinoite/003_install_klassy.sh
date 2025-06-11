f2a4eba8f25edb75058b740c8778663681a31f47
tazihad <tazihad@gmail.com>
2025-06-11 13:54:42 +0600

    use rpm-ostree to install

--- scripts/kinoite/003_install_klassy.sh
+++ scripts/kinoite/003_install_klassy.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-dnf config-manager --add-repo "https://download.opensuse.org/repositories/home:paul4us/Fedora_$(rpm -E %fedora)/home:paul4us.repo"
-dnf install klassy -y
+RELEASE=$(rpm -E %fedora)
+
+curl -fsSL -o /tmp/home-paul4us-fedora-"${RELEASE}".repo \
+"https://download.opensuse.org/repositories/home:paul4us/Fedora_${RELEASE}/home:paul4us.repo"
+
+sudo mv /tmp/home-paul4us-fedora-"${RELEASE}".repo \
+/etc/yum.repos.d/home:paul4us.repo
+
+# Install klassy from the repository
+rpm-ostree install klassy
+
+# Remove the repository file after installation
+rm -f /etc/yum.repos.d/home:paul4us.repo
+
+
