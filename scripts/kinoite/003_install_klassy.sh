e2568af0a5d254670f5237cd765c703a41f2732a
tazihad <tazihad@gmail.com>
2025-06-11 12:54:06 +0600

    dynamic version

--- scripts/kinoite/003_install_klassy.sh
+++ scripts/kinoite/003_install_klassy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_42/home:paul4us.repo
+dnf config-manager --add-repo "https://download.opensuse.org/repositories/home:paul4us/Fedora_$(rpm -E %fedora)/home:paul4us.repo"
 dnf install klassy -y
