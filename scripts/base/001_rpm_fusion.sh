895e1e758013f722535ae1a07ec5f65d478e7a14
tazihad <tazihad@gmail.com>
2025-08-30 02:52:13 +0600

    rpmfusion fix

--- scripts/base/001_rpm_fusion.sh
+++ scripts/base/001_rpm_fusion.sh
@@ -3,10 +3,10 @@
 set -euox pipefail
 
 # Setup RPMFusion repositories
-dnf install \
+dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-  
+
 # Update repo metadata
 dnf -y update rpmfusion-free-release rpmfusion-nonfree-release
 
