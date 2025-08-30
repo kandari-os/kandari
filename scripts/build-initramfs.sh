52d50409a11b1627711e189ac0af2bc2b17630a0
tazihad <tazihad@gmail.com>
2025-08-30 17:08:29 +0600

    fix initramfs

--- scripts/build-initramfs.sh
+++ scripts/build-initramfs.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-set -oue pipefail
+set -euox pipefail
 
-# No need for rpm-ostree cliwrap, dracut is available directly
+dnf config-manager setopt fedora-multimedia.enabled=0
+
+# Generate initramfs
+mkdir -p /var/tmp
 KERNEL_SUFFIX=""
-
-# Find installed kernel version
-QUALIFIED_KERNEL="$(rpm -qa | grep -P "kernel(|${KERNEL_SUFFIX})(-\d+\.\d+\.\d+)" | sed -E "s/kernel(|${KERNEL_SUFFIX})-//")"
-
-# Generate initramfs for that kernel
-dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-# Secure permissions
-chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
